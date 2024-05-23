ce8cd2ca70cea7bbfa8a45720bf6c61d75c6e935
razi17571 <ansari17571@gmail.com>
2024-05-23 22:02:45 +0530

    Initial Commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import CategoryList from '../components/CategoryList'
+import BannerProduct from '../components/BannerProduct'
+import HorizontalCardProduct from '../components/HorizontalCardProduct'
+import VerticalCardProduct from '../components/VerticalCardProduct'
+
+const Home = () => {
+  return (
+    <div className='over overflow-y-scroll scrollbar-none'>
+      <CategoryList/>
+      <BannerProduct/>
+
+      <HorizontalCardProduct category={"airpodes"} heading={"Top's Airpodes"}/>
+      <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+      
+      <VerticalCardProduct category={"mobile"} heading={"Mobiles"}/>
+      <VerticalCardProduct category={"mouse"} heading={"Mouse"}/>
+      <VerticalCardProduct category={"TV"} heading={"Televisions"}/>
+      <VerticalCardProduct category={"camera"} heading={"Camera & Photography"}/>
+      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
+      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
+      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
+    </div>
+  )
+}
+
+export default Home
